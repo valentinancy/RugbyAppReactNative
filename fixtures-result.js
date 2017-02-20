fc8fa27f4a799ac7ffe1aa02b893a711705f413f
Stephanie Tania <Stephanie Tania>
2017-02-20 21:55:35 +0700

    Penambahan untuk Fixtures & Result

---
+++ fixtures-result.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import {Text} from 'react-native';
+
+export default class FixturesResult extends Component{
+	render(){
+		return(
+			<Text>Halaman untuk Fixtures & Result</Text>
+		);
+	}
+}
