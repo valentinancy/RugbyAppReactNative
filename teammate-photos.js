34fd404d1546b400aeb2b0b13abcd8de447c1f06
claraccs <clara020896@gmail.com>
2017-02-21 13:11:36 +0700

    tambah halaman teammate photos

---
+++ teammate-photos.js
@@ -0,0 +1,10 @@
+import React,{Component} from 'react';
+import {Text} from 'react-native';
+
+export default class TeammatePhotos extends Component{
+	render(){
+		return(
+			<Text>Halaman untuk Teammate Photos</Text>
+		);
+	}
+}
