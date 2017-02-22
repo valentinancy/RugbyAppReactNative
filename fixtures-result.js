1ef195211e0179c3d982b46add43e11afb207156
StephanieTania <7314072@student.unpar.ac.id>
2017-02-22 12:35:00 +0700

    Tambahan konten halaman

--- fixtures-result.js
+++ fixtures-result.js
@@ -1,10 +1,40 @@
 import React,{Component} from 'react';
-import {Text} from 'react-native';
+import {AppRegistry,Image,View,StyleSheet,Dimensions} from 'react-native';
+
+var screenWidth=Dimensions.get('window').width;
 
 export default class FixturesResult extends Component{
-	render(){
-		return(
-			<Text>Halaman untuk Fixtures & Result</Text>
-		);
-	}
+  render(){
+    let banner1={
+      uri:'http://rugbyindonesia.or.id/wp-content/uploads/2016/03/1-Jakarta-XV-Banner-1024x343.jpg'
+    }
+    let banner2={
+      uri:'http://rugbyindonesia.or.id/wp-content/uploads/2016/03/2-Jakarta-10s-Banner-1024x343.jpg'
+    }
+    let banner3={
+      uri:'http://rugbyindonesia.or.id/wp-content/uploads/2016/03/3-Nusnatara-7s-1024x343.jpg'
+    }    
+    return(
+      <View style={styles.container} >
+        <Image source={banner1} style={styles.imgBanner}/>
+        <Image source={banner2} style={styles.imgBanner}/>
+        <Image source={banner3} style={styles.imgBanner}/>        
+      </View>
+    );
+  }
 }
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:'column',
+    margin:15,
+  },
+  imgBanner:{
+    flex:1,
+    width: screenWidth-30,
+    height: null,
+    resizeMode:'contain',
+    marginVertical:30,
+  }
+});
