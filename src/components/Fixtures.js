8b06b2576c1cdbe61c4f2385e10b9a391dfa6e2f
StephanieTania <7314072@student.unpar.ac.id>
2017-02-23 23:00:36 +0700

    Memindahkan kode untuk halaman Fixtures

--- src/components/Fixtures.js
+++ src/components/Fixtures.js
@@ -1,15 +1,42 @@
-import React, { Component } from 'react'
-import { View,Image,Text } from 'react-native'
+import React,{Component} from 'react';
+import {AppRegistry,Image,View,StyleSheet,Dimensions} from 'react-native';
 
+var screenWidth=Dimensions.get('window').width;
 
-class Fixtures extends Component {
-    render() {
-        return(
-            <View>
-                <Text>This is Fixtures</Text>
-            </View>
-        )
+class Fixtures extends Component{
+  render(){
+    let banner1={
+      uri:'http://rugbyindonesia.or.id/wp-content/uploads/2016/03/1-Jakarta-XV-Banner-1024x343.jpg'
     }
+    let banner2={
+      uri:'http://rugbyindonesia.or.id/wp-content/uploads/2016/03/2-Jakarta-10s-Banner-1024x343.jpg'
+    }
+    let banner3={
+      uri:'http://rugbyindonesia.or.id/wp-content/uploads/2016/03/3-Nusnatara-7s-1024x343.jpg'
+    }    
+    return(
+      <View style={styles.container} >
+        <Image source={banner1} style={styles.imgBanner}/>
+        <Image source={banner2} style={styles.imgBanner}/>
+        <Image source={banner3} style={styles.imgBanner}/>        
+      </View>
+    );
+  }
 }
+
+const styles=StyleSheet.create({
+  container:{
+    flex:1,
+    flexDirection:'column',
+    margin:15,
+  },
+  imgBanner:{
+    flex:1,
+    width: screenWidth-30,
+    height: null,
+    resizeMode:'contain',
+    marginVertical:30,
+  }
+});
 
 export default Fixtures
