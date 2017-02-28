87bc88e8293ecad00a2c810f7674e9a8447bfa77
LydiaFebtriani <7314044@student.unpar.ac.id>
2017-02-28 15:21:26 +0700

    Menambah beberapa method

--- src/components/ReadMoreNews.js
+++ src/components/ReadMoreNews.js
@@ -26,13 +26,60 @@ class ReadMoreNews extends Component{
     console.log('data received', webViewData, jsonData);
   }
  
-  render() {
+  readMore(no){
+    switch(no){
+      case 1:
+        render1();
+        break;
+      case 2:
+        render2();
+        break;
+      case 3:
+        render3();
+        break;
+      case 4:
+        render4();
+        break;
+    }
+  }
+
+  render1() {
     return (
       <WebViewBridge
         ref="webviewbridge"
         onBridgeMessage={this.onBridgeMessage.bind(this)}
         injectedJavaScript={injectScript}
-        source={{uri: "http://google.com"}}/>
+        source={{uri: "http://rugbyindonesia.or.id/2016-indonesian-national-team-the-rhinos-squad-announcement"}}/>
+    );
+  }
+
+  render2() {
+    return (
+      <WebViewBridge
+        ref="webviewbridge"
+        onBridgeMessage={this.onBridgeMessage.bind(this)}
+        injectedJavaScript={injectScript}
+        source={{uri: "http://rugbyindonesia.or.id/srikandi-sevens-tournament-and-get-into-rugby-jagorawi"}}/>
+    );
+  }
+
+  render3() {
+    return (
+      <WebViewBridge
+        ref="webviewbridge"
+        onBridgeMessage={this.onBridgeMessage.bind(this)}
+        injectedJavaScript={injectScript}
+        source={{uri: "http://rugbyindonesia.or.id/papua-sevens-rugby-dominates-historic-rugby-event"}}/>
+    );
+  }
+
+  render4() {
+    return (
+      <WebViewBridge
+        ref="webviewbridge"
+        onBridgeMessage={this.onBridgeMessage.bind(this)}
+        injectedJavaScript={injectScript}
+        source={{uri: "http://rugbyindonesia.or.id/indonesian-rugby-mobile-application-launched"}}/>
     );
   }
 }
