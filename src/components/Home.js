439d49d1e160004253a071c54951e0cfbf67a0cc
valentinancy <nancy_valentina@ymail.com>
2017-03-05 01:38:21 +0700

    change home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,6 +3,7 @@ import { View,Image,Text,TouchableOpacity } from 'react-native'
 import { Actions } from 'react-native-router-flux'
 import SideMenu from 'react-native-side-menu'
 import Menu from './Menu'
+import News from './News'
 
 /* KELAS BUTTON NAVIGASI */
 class Button extends Component {
@@ -48,28 +49,6 @@ class Home extends Component {
       }
     /* END MENU */
 
-    handleClick(e,page) {
-        switch(page) {
-            case 'news':
-                Actions.news()
-                break
-            case 'fixtures':
-                Actions.fixtures()
-                break
-            case 'teammate':
-                Actions.teammate()
-                break
-            case 'rugbyClubs':
-                Actions.rugbyClubs()
-                break
-            case 'rugby101':
-                Actions.rugby101()
-                break
-            default:
-                Actions.home()
-        }
-    }
-
     render() {
         //const { nav } = styles
         const menu = <Menu onItemSelected={this.onMenuItemSelected} />;
@@ -80,12 +59,9 @@ class Home extends Component {
             isOpen={this.state.isOpen}
             onChange={(isOpen) => this.updateMenuState(isOpen)}>
             <View style={styles.container}>
-                    <Text onPress={ (e) => this.handleClick(e,'news') }>News</Text>
-                    <Text onPress={ (e) => this.handleClick(e,'fixtures') }>Fixtures and Result</Text>
-                    <Text onPress={ (e) => this.handleClick(e,'teammate') }>Teammate Photos</Text>
-                    <Text onPress={ (e) => this.handleClick(e,'rugbyClubs') }>Rugby Clubs</Text>
-                    <Text onPress={ (e) => this.handleClick(e,'rugby101') }>Rugby 101</Text>
+              <News />
             </View>
+
             <Button style={styles.button} onPress={() => this.toggle()}>
               <Image
                 source={require('../../assets/images/menu.png')} style={{width: 70, height: 70}} />
@@ -96,21 +72,18 @@ class Home extends Component {
 }
 
 const styles = {
-    nav: {
-        marginTop: 55
-    },
     button: {
         position: 'absolute',
         top: 50,
         padding: 10,
       },
-  container: {
-    flex: 1,
-    marginTop: 55,
-    //justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#F5FCFF',
-  },
+    container: {
+      flex: 1,
+      marginTop: 55,
+      //justifyContent: 'center',
+      alignItems: 'center',
+      backgroundColor: '#F5FCFF',
+    }
 }
 
 export default Home
