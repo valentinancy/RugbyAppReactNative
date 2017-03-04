b2295b7aa1ae0a7bcf8b8fee9a692c59a479210b
valentinancy <nancy_valentina@ymail.com>
2017-03-05 02:38:15 +0700

    complete the rugby 101 page

--- src/components/Rugby101.js
+++ src/components/Rugby101.js
@@ -1,11 +1,41 @@
 import React, { Component } from 'react'
-import { Text } from 'react-native'
+import { Text, ScrollView, Image, View } from 'react-native'
 
 class Rugby101 extends Component {
     render() {
+        const { titlePage, headerImg, container } = styles
+
         return(
-            <Text>Coming soon!</Text>
+            <ScrollView>
+                <Image style={ headerImg }
+                    source={ require('../../assets/images/sub-header-101.png') }>
+                    <View>
+                        <Text style={ titlePage }>RUGBY 101</Text>
+                    </View>
+                </Image>
+                <View style={ container }>
+                    <Text>Coming soon!</Text>
+                </View>
+            </ScrollView>
         )
+    }
+}
+
+const styles = {
+    headerImg: {
+        height: 100,
+        width: 360
+    },
+    titlePage: {
+        fontSize: 30,
+        textAlign: 'center',
+        marginTop: 50,
+        color: '#f0f8ff',
+        fontWeight: 'bold'
+    },
+    container: {
+        marginTop: 10,
+        marginLeft: 10
     }
 }
 
