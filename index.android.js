e29729b084be8ca72b5b5feab573c4b6f0f85687
StephanieTania <7314072@student.unpar.ac.id>
2017-03-06 22:46:35 +0700

    Perbaikan posisi logo, tambah button kiri

--- index.android.js
+++ index.android.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { View, Text, AppRegistry, Navigator,TouchableHighlight,Image } from 'react-native'
-import { Scene, Router } from 'react-native-router-flux'
+import { Scene, Router, Actions } from 'react-native-router-flux'
 import Fixtures from './src/components/Fixtures'
 import Home from './src/components/Home'
 import News from './src/components/News'
@@ -12,19 +12,22 @@ import Rugby101 from './src/components/Rugby101'
 class App extends React.Component {
   render() {
     return(
-        <Router sceneStyle={{ paddingTop: 55 }}>
-            <Scene key="root"
-                navigationBarBackgroundImage={require('./assets/images/header.png')}
-                renderTitle={() => (<Image source={require('./assets/images/logo-header.png')}style={{width:null,height:30,resizeMode:'contain',marginVertical:13}} />)}>
-                <Scene key="home" component={ Home } initial/>
-                <Scene key="fixtures" component={ Fixtures }/>
-                <Scene key="news" component={ News }/>
-                <Scene key="readMoreNews" component={ ReadMoreNews }/>
-                <Scene key="teammate" component={ Teammate }/>
-                <Scene key="rugbyClubs" component={ RugbyClubs }/>
-                <Scene key="rugby101" component={ Rugby101 }/>
-            </Scene>
-        </Router>
+        <Router sceneStyle={{ paddingTop: 55 }}
+            navigationBarBackgroundImage={require('./assets/images/header.png')}
+            renderTitle={() => (<Image source={require('./assets/images/logo-header.png')} style={{height:40,resizeMode:'contain',marginVertical:7}} />)}            
+            /*renderBackButton={() => (null)}*/
+            renderLeftButton={() => (<Image source={require('./assets/images/menu.png')} style={{height:20,resizeMode:'contain',marginVertical:7,marginLeft:-40}} />)}
+            onLeft={() => (Actions.news())}
+            renderRightButton={() => (<Image source={require('./assets/images/menu.png')} style={{height:20,resizeMode:'contain',marginVertical:7}} />)}
+            onRight={() => (Actions.fixtures())}>
+            <Scene key="home" component={ Home } initial/>
+            <Scene key="fixtures" component={ Fixtures }/>
+            <Scene key="news" component={ News }/>
+            <Scene key="readMoreNews" component={ ReadMoreNews }/>
+            <Scene key="teammate" component={ Teammate }/>
+            <Scene key="rugbyClubs" component={ RugbyClubs }/>
+            <Scene key="rugby101" component={ Rugby101 }/>            
+    </Router>
     )
   }
 }
